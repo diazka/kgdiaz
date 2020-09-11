8e108fd19df5c6360d61118af96cd53435a155da
Daniela Garcia <55029989+danielagarcia9@users.noreply.github.com>
2020-09-11 11:14:34 -0500

    New navbar

---
+++ app.js
@@ -0,0 +1,23 @@
+const navSlide = () => {
+    const menu = document.querySelector('.menu');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    menu.addEventListener('click', () => {
+        //Toggle Nav
+        nav.classList.toggle('nav-active');
+
+        //Animate Links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forward ${index / 7 + 1.5}s`
+            }
+         })
+         //Menu animation
+         menu.classList.toggle('toggle');
+    });
+}
+
+navSlide()
