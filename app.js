09ffa18bfb9b7cc7a594a4aa02c39ef34c77e4a4
Daniela Garcia <55029989+danielagarcia9@users.noreply.github.com>
2020-09-15 18:12:56 -0500

    Ajustes (sticky oculto)

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// NAVIGATION BAR
 const navSlide = () => {
     const menu = document.querySelector('.menu');
     const nav = document.querySelector('.nav-links');
@@ -22,17 +23,17 @@ const navSlide = () => {
 
 navSlide()
 
+
+// SCROLL TO TOP BUTTON
 //Get the button:
-var mybutton = document.getElementById("up-button");
-
+const myButton = document.getElementById('up-button');
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
+window.onscroll = function scrollFunction() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
+    myButton.style.display = "block";
   } else {
-    mybutton.style.display = "none";
+    myButton.style.display = "none";
   }
 }
 
@@ -41,3 +42,24 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+
+
+/*// STICKY NAVBAR
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}*/
