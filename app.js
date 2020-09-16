8078257b009dc7919e4ea4e8efad91fef3174c13
Daniela Garcia <55029989+danielagarcia9@users.noreply.github.com>
2020-09-16 10:22:06 -0500

    Pre text-decoration optimization

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const navSlide = () => {
     const menu = document.querySelector('.menu');
     const nav = document.querySelector('.nav-links');
     const navLinks = document.querySelectorAll('.nav-links li');
-
     menu.addEventListener('click', () => {
         //Toggle Nav
         nav.classList.toggle('nav-active');
@@ -20,15 +19,13 @@ const navSlide = () => {
          menu.classList.toggle('toggle');
     });
 }
-
 navSlide()
 
 
 // SCROLL TO TOP BUTTON
-//Get the button:
 const myButton = document.getElementById('up-button');
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function scrollFunction() {scrollFunction()};
+window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     myButton.style.display = "block";
@@ -36,7 +33,6 @@ function scrollFunction() {
     myButton.style.display = "none";
   }
 }
-
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
